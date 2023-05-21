2882d2a9318fb2fed4a9a3ea93bb1ebd17e88a56
Masoud Afrashteh <massoud.afrashteh@gmail.com>
2023-05-22 00:56:58 +0200

    Set up H2 database

--- src/main/java/com/example/demo/model/Ingredient.java
+++ src/main/java/com/example/demo/model/Ingredient.java
@@ -1,6 +1,9 @@
 package com.example.demo.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -10,6 +13,10 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 public class Ingredient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
     private String name;
 }
